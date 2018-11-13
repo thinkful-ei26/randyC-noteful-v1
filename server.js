6090217ed2e51cdd4efd9ccc33db340bddae5484
R-Cole <randy@goodglue.com>
2018-11-13 12:24:02 -0700

    updated files

--- server.js
+++ server.js
@@ -23,25 +23,41 @@ app.listen(8080, function(){
 });
 
 //End points
+
+//Get all notes and Search is a search term...
 app.get('/api/notes',(req,res) => {
 
-  res.json(data);
+  const currentSearchTerm = req.query.searchTerm;
 
+  if(currentSearchTerm !== undefined){
+
+    //filter for title
+    const noteBySearchTerm = data.filter(note => note.title.includes(req.query.searchTerm));
+
+    res.json(noteBySearchTerm);
+
+  }else{
+
+    res.json(data);
+ 
+  }
+ 
 });
 
 
 app.get('/api/notes/:id',(req,res) => {
-
-  console.log(typeof req.params.id);
-
+ 
+  //gets a note by params id (converted to a number)
   const noteById = data.find(note => note.id === parseInt(req.params.id));
-
-  console.log(noteById);
-
-  
+ 
   res.json(noteById);
 
 });
+
+ 
+
+  
+ 
 
 
  
