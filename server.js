78bd80a6505818a0ba8f35af5da9bbae8327723b
R-Cole <randy@goodglue.com>
2018-11-13 14:34:50 -0700

    updated server.js

--- server.js
+++ server.js
@@ -2,6 +2,14 @@
 
 console.log('Hello Noteful!');
 
+//Simple in-memory database
+const data = require('./db/notes');
+
+const simDB = require('./db/simDB');
+
+const notes = simDB.initialize(data);
+
+
 const { PORT } = require('./config');
 
 const accessLogging = require('./middleware/logger');
@@ -13,20 +21,45 @@ const express = require('express');
 
 // Load array of notes
 
-//Simple in-memory database
-const data = require('./db/notes');
-
-const simDB = require('./db/simDB');
-
-const notes = simDB.initialize(data);
 
 
 
 const app = express();
 
+app.use(express.static('public'));
+
 // ADD STATIC SERVER HERE
 
 app.use(accessLogging);
+
+
+//End points
+
+//Get all notes and Search titles using a search term...
+app.get('/api/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+
+  });
+});
+
+
+app.get('/api/notes/:id',(req,res) => {
+ 
+  //gets a note by params id (converted to a number)
+  const noteById = data.find(note => note.id === parseInt(req.params.id));
+ 
+  res.json(noteById);
+
+});
+
+ 
+
 
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
@@ -42,6 +75,10 @@ app.use(function (err, req, res, next) {
   });
 });
 
+
+
+
+
  
 
 app.listen(PORT, function(){
@@ -53,47 +90,6 @@ app.listen(PORT, function(){
   console.error(err);
 
 });
-
-//End points
-
-//ERROR END POINT
-app.get('/boom', (req, res, next) => {
-  throw new Error('Boom!!');
-});
-
-//Get all notes and Search is a search term...
-app.get('/api/notes',(req,res) => {
-
-  const currentSearchTerm = req.query.searchTerm;
-
-  if(currentSearchTerm !== undefined){
-
-    //filter for title
-    const noteBySearchTerm = data.filter(note => note.title.includes(req.query.searchTerm));
-
-    res.json(noteBySearchTerm);
-
-  }else{
-
-    res.json(data);
- 
-  }
- 
-});
-
-
-app.get('/api/notes/:id',(req,res) => {
- 
-  //gets a note by params id (converted to a number)
-  const noteById = data.find(note => note.id === parseInt(req.params.id));
- 
-  res.json(noteById);
-
-});
-
- 
-
-  
  
 
 
